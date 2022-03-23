80d538a3a8d5a854e98a2598d9baa1cc4d44f01f
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 17:18:31 -0500

    Se realiza implementacion de ReturnResource en ReturnResourceImp

--- src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseCommandImp/ReturnResourceImp.java
+++ src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseCommandImp/ReturnResourceImp.java
@@ -1,4 +1,21 @@
 package com.example.bibliotecareactiva.application.useCaseImp.useCaseCommandImp;
 
-public class ReturnResourceImp {
+import com.example.bibliotecareactiva.application.mappers.ResourceMapper;
+import com.example.bibliotecareactiva.application.useCase.useCaseCommandInterface.ReturnResource;
+import com.example.bibliotecareactiva.domain.repository.ResourceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Service
+public class ReturnResourceImp implements ReturnResource {
+
+    private final ResourceRepository resourceRepository;
+    private final ResourceMapper resourceMapper;
+
+    @Override
+    public Mono<String> returnResource(String id) {
+        return null;
+    }
 }
