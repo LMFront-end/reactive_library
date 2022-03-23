5ea1b925a6d13a156b3c205133c4f5f92c3bf5e0
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 13:03:28 -0500

    se crea archivos en utils

---
+++ src/main/java/com/example/bibliotecareactiva/domain/utils/Messages.java
@@ -0,0 +1,10 @@
+package com.example.bibliotecareactiva.domain.utils;
+
+public class Messages {
+
+    public static final String RESOURCE_NOT_FOUND = "The resource searching with the id, not found.";
+    public static final String RESOURCE_NOT_FOUND_NOT_AVAILABLE = "The resource wasn't found or is unavailable";
+    public static final String RESOURCE_NOT_FOUND_AVAILABLE = "The resource wasn't found or is available";
+    public static final String BORROWED_SUCCESSFULLY = "The resource was successfully provided";
+
+}
