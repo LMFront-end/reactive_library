1716f37ab513ec2fb9e584337b31730b73a87a58
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 17:39:33 -0500

    se implementa ListResource en ListResourceImp

--- src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseQueryImp/ListResourceImp.java
+++ src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseQueryImp/ListResourceImp.java
@@ -1,4 +1,24 @@
 package com.example.bibliotecareactiva.application.useCaseImp.useCaseQueryImp;
 
-public class ListResourceImp {
+import com.example.bibliotecareactiva.application.mappers.ResourceMapper;
+import com.example.bibliotecareactiva.application.useCase.usecaseQueryInterface.ListResource;
+import com.example.bibliotecareactiva.domain.dto.ResourceDTO;
+import com.example.bibliotecareactiva.domain.repository.ResourceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ListResourceImp implements ListResource {
+
+    private final ResourceRepository resourceRepository;
+    private final ResourceMapper resourceMapper;
+
+    @Override
+    public Flux<List<ResourceDTO>> listResource() {
+        return null;
+    }
 }
