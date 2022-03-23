088e8fb63f51890016f032c68977386ad663277c
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 17:26:32 -0500

    Se realiza implementacion de FindByAuthor en FindByAuthorImp

--- src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseQueryImp/FindByAuthorImp.java
+++ src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseQueryImp/FindByAuthorImp.java
@@ -1,4 +1,24 @@
 package com.example.bibliotecareactiva.application.useCaseImp.useCaseQueryImp;
 
-public class FindByAuthorImp {
+import com.example.bibliotecareactiva.application.mappers.ResourceMapper;
+import com.example.bibliotecareactiva.application.useCase.usecaseQueryInterface.FindByAuthor;
+import com.example.bibliotecareactiva.domain.dto.ResourceDTO;
+import com.example.bibliotecareactiva.domain.repository.ResourceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class FindByAuthorImp implements FindByAuthor {
+
+    private final ResourceRepository resourceRepository;
+    private final ResourceMapper resourceMapper;
+
+    @Override
+    public Flux<List<ResourceDTO>> findByAuthor(String author) {
+        return null;
+    }
 }
