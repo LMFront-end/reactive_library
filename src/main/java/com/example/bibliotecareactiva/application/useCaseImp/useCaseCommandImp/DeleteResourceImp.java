46fd6869f3ad9f9a6a6cb2f2e10cc0630ca117b1
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 17:16:59 -0500

    Se realiza implementacion de DeleteResource en DeleteResourceImp

--- src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseCommandImp/DeleteResourceImp.java
+++ src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseCommandImp/DeleteResourceImp.java
@@ -1,4 +1,21 @@
 package com.example.bibliotecareactiva.application.useCaseImp.useCaseCommandImp;
 
-public class DeleteResourceImp {
+import com.example.bibliotecareactiva.application.mappers.ResourceMapper;
+import com.example.bibliotecareactiva.application.useCase.useCaseCommandInterface.DeleteResource;
+import com.example.bibliotecareactiva.domain.repository.ResourceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Service
+public class DeleteResourceImp implements DeleteResource {
+
+    private final ResourceRepository resourceRepository;
+    private final ResourceMapper resourceMapper;
+
+    @Override
+    public Mono<Void> deleteClearData() {
+        return null;
+    }
 }
