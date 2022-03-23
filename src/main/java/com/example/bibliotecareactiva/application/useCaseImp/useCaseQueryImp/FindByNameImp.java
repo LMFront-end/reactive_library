6bca800292f2da763532d64bbdaee6d514fd73e4
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 17:38:04 -0500

    se implementa FindByName en FindByNameImp

--- src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseQueryImp/FindByNameImp.java
+++ src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseQueryImp/FindByNameImp.java
@@ -1,4 +1,24 @@
 package com.example.bibliotecareactiva.application.useCaseImp.useCaseQueryImp;
 
-public class FindByNameImp {
+import com.example.bibliotecareactiva.application.mappers.ResourceMapper;
+import com.example.bibliotecareactiva.application.useCase.usecaseQueryInterface.FindByName;
+import com.example.bibliotecareactiva.domain.dto.ResourceDTO;
+import com.example.bibliotecareactiva.domain.repository.ResourceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class FindByNameImp implements FindByName {
+
+    private final ResourceRepository resourceRepository;
+    private final ResourceMapper resourceMapper;
+
+    @Override
+    public Flux<List<ResourceDTO>> findByName(String name) {
+        return null;
+    }
 }
