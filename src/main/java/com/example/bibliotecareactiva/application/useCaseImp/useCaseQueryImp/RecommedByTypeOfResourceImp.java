60e700dd89eab499a66ed4eaea1af4f6c89b9957
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 17:41:37 -0500

    se implementa RecommendByTypeOfResource en RecommendByTypeOfResourceImp

--- src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseQueryImp/RecommedByTypeOfResourceImp.java
+++ src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseQueryImp/RecommedByTypeOfResourceImp.java
@@ -1,4 +1,25 @@
 package com.example.bibliotecareactiva.application.useCaseImp.useCaseQueryImp;
 
-public class RecommedByTypeOfResourceImp {
+import com.example.bibliotecareactiva.application.mappers.ResourceMapper;
+import com.example.bibliotecareactiva.application.useCase.usecaseQueryInterface.RecommendByTypeOfResource;
+import com.example.bibliotecareactiva.domain.dto.ResourceDTO;
+import com.example.bibliotecareactiva.domain.repository.ResourceRepository;
+import com.example.bibliotecareactiva.domain.valueObject.TypeOfResource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class RecommedByTypeOfResourceImp implements RecommendByTypeOfResource {
+
+    private final ResourceRepository resourceRepository;
+    private final ResourceMapper resourceMapper;
+
+    @Override
+    public Flux<List<ResourceDTO>> recommendByTypeOfResource(TypeOfResource typeOfResource) {
+        return null;
+    }
 }
