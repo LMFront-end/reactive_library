63dd5b2a2d3105d1473acf2300f201042b7437d3
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 17:20:46 -0500

    Se realiza implementacion de SaveResource en SaveResourceImp

--- src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseCommandImp/SaveResourceImp.java
+++ src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseCommandImp/SaveResourceImp.java
@@ -1,4 +1,22 @@
 package com.example.bibliotecareactiva.application.useCaseImp.useCaseCommandImp;
 
-public class SaveResourceImp {
+import com.example.bibliotecareactiva.application.mappers.ResourceMapper;
+import com.example.bibliotecareactiva.application.useCase.useCaseCommandInterface.SaveResource;
+import com.example.bibliotecareactiva.domain.dto.ResourceDTO;
+import com.example.bibliotecareactiva.domain.repository.ResourceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Service
+public class SaveResourceImp implements SaveResource {
+
+    private final ResourceRepository resourceRepository;
+    private final ResourceMapper resourceMapper;
+
+    @Override
+    public Mono<ResourceDTO> saveResource(ResourceDTO resourceDTO) {
+        return null;
+    }
 }
