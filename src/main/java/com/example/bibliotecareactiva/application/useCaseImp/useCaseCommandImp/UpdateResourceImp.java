dc788b5ef099dd34ac72eeaceff372c726f6ac3a
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 17:22:17 -0500

    Se realiza implementacion de UpdateResource en UpdateResourceImp

--- src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseCommandImp/UpdateResourceImp.java
+++ src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseCommandImp/UpdateResourceImp.java
@@ -1,4 +1,22 @@
 package com.example.bibliotecareactiva.application.useCaseImp.useCaseCommandImp;
 
-public class UpdateResourceImp {
+import com.example.bibliotecareactiva.application.mappers.ResourceMapper;
+import com.example.bibliotecareactiva.application.useCase.useCaseCommandInterface.UpdateResource;
+import com.example.bibliotecareactiva.domain.dto.ResourceDTO;
+import com.example.bibliotecareactiva.domain.repository.ResourceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Service
+public class UpdateResourceImp implements UpdateResource {
+
+    private final ResourceRepository resourceRepository;
+    private final ResourceMapper resourceMapper;
+
+    @Override
+    public Mono<ResourceDTO> updateResource(ResourceDTO resourceDTO) {
+        return null;
+    }
 }
