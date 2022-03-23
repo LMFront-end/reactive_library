ab6a0d06d47d57963a06af425802c20a45c757af
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 13:11:08 -0500

    se eliminan carpeta validation

--- src/main/java/com/example/bibliotecareactiva/application/validation/ResourceDTOValidation.java
+++
@@ -1,6 +0,0 @@
-package com.example.bibliotecareactiva.application.validation;
-
-import com.example.bibliotecareactiva.domain.dto.ResourceDTO;
-
-public class ResourceDTOValidation {
-}
--- src/main/java/com/example/bibliotecareactiva/config/WebFluxConfig.java
+++ src/main/java/com/example/bibliotecareactiva/config/WebFluxConfig.java
@@ -1,4 +1,4 @@
-package com.example.bibliotecareactiva.configuration;
+package com.example.bibliotecareactiva.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
