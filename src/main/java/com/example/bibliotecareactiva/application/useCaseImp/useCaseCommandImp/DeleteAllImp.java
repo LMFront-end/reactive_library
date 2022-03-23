81cc218c7d1d080e1d1bbfe62f9cb0712fbef844
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 17:09:28 -0500

    Se realiza implementacion de DeleteAll en DeleteAllImp

--- src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseCommandImp/DeleteAllImp.java
+++ src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseCommandImp/DeleteAllImp.java
@@ -1,4 +1,21 @@
 package com.example.bibliotecareactiva.application.useCaseImp.useCaseCommandImp;
 
-public class DeleteAllImp {
+import com.example.bibliotecareactiva.application.mappers.ResourceMapper;
+import com.example.bibliotecareactiva.application.useCase.useCaseCommandInterface.DeleteAll;
+import com.example.bibliotecareactiva.domain.repository.ResourceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Service
+public class DeleteAllImp implements DeleteAll {
+
+    private final ResourceRepository resourceRepository;
+    private final ResourceMapper resourceMapper;
+
+    @Override
+    public Mono<Void> deleteById(String id) {
+        return null;
+    }
 }
