7359e9159ac31c812d1f18bebe5387db37dabe1b
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 13:15:57 -0500

    se crea JsonReader

--- src/main/java/com/example/bibliotecareactiva/application/JsonReader.java
+++ src/main/java/com/example/bibliotecareactiva/application/JsonReader.java
@@ -1,4 +1,27 @@
 package com.example.bibliotecareactiva.application;
 
+import com.example.bibliotecareactiva.domain.dto.ResourceDTO;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Getter
 public class JsonReader {
+
+    private final String path = "src/main/java/com/example/bibliotecareactiva/domain/utils/Data.json";
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private List<ResourceDTO> resourceDTOS;
+
+    public JsonReader(){
+        try{
+            resourceDTOS = objectMapper.readValue(new File(path), new TypeReference<>() {});
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
