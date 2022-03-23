aeef3a1b2f0911eb10839087dc544d2dd2929b52
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 16:31:57 -0500

    en la clase ResourceMapper se cra Function mapToDTO

--- src/main/java/com/example/bibliotecareactiva/application/mappers/ResourceMapper.java
+++ src/main/java/com/example/bibliotecareactiva/application/mappers/ResourceMapper.java
@@ -1,7 +1,24 @@
 package com.example.bibliotecareactiva.application.mappers;
 
+import com.example.bibliotecareactiva.domain.collections.Resource;
+import com.example.bibliotecareactiva.domain.dto.ResourceDTO;
 import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
 
 @Component
 public class ResourceMapper {
+
+    public Function<Resource, ResourceDTO> mapToDTO(){
+        return resource -> new ResourceDTO(
+                resource.getId(),
+                resource.getName(),
+                resource.getAuthor(),
+                resource.getAvailability(),
+                resource.getLastBorrowed(),
+                resource.getThematicArea(),
+                resource.getTypeOfResource()
+        );
+
+    }
 }
