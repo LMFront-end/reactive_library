1b37988d14409fd58019982a3ca2e8908c7aa1d2
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 17:07:48 -0500

    Se realiza implementacion de BorrowResource en BorrowResourceImp

--- src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseCommandImp/BorrowResourceImp.java
+++ src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseCommandImp/BorrowResourceImp.java
@@ -1,4 +1,21 @@
 package com.example.bibliotecareactiva.application.useCaseImp.useCaseCommandImp;
 
-public class BorrowResourceImp {
+import com.example.bibliotecareactiva.application.mappers.ResourceMapper;
+import com.example.bibliotecareactiva.application.useCase.useCaseCommandInterface.BorrowResource;
+import com.example.bibliotecareactiva.domain.repository.ResourceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Service
+public class BorrowResourceImp implements BorrowResource {
+
+    private final ResourceRepository resourceRepository;
+    private final ResourceMapper resourceMapper;
+
+    @Override
+    public Mono<String> borrowResource(String id) {
+        return null;
+    }
 }
