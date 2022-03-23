a2bebf3d55c2e1b3bd8eeb8c6ee1d53fe7b1f0ec
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 17:42:58 -0500

    se implementa RecommendByThematicArea en RecommendThematicAreaImp

--- src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseQueryImp/RecommendByThematicAreaImp.java
+++ src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseQueryImp/RecommendByThematicAreaImp.java
@@ -1,4 +1,25 @@
 package com.example.bibliotecareactiva.application.useCaseImp.useCaseQueryImp;
 
-public class RecommendByThematicAreaImp {
+import com.example.bibliotecareactiva.application.mappers.ResourceMapper;
+import com.example.bibliotecareactiva.application.useCase.usecaseQueryInterface.RecommendByThematicArea;
+import com.example.bibliotecareactiva.domain.dto.ResourceDTO;
+import com.example.bibliotecareactiva.domain.repository.ResourceRepository;
+import com.example.bibliotecareactiva.domain.valueObject.ThematicArea;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class RecommendByThematicAreaImp implements RecommendByThematicArea {
+
+    private final ResourceRepository resourceRepository;
+    private final ResourceMapper resourceMapper;
+
+    @Override
+    public Flux<List<ResourceDTO>> recommendByThematicArea(ThematicArea thematicArea) {
+        return null;
+    }
 }
