6f1605eea73fe53ded931f69f3b45302027c6982
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 20:33:09 -0500

    se implementa ThematicAreaAndTypeOfResource en ThematicAreaAndTypeOfResourceImp

--- src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseQueryImp/ThematicAreaAndTypeOfResourceImp.java
+++ src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseQueryImp/ThematicAreaAndTypeOfResourceImp.java
@@ -1,4 +1,26 @@
 package com.example.bibliotecareactiva.application.useCaseImp.useCaseQueryImp;
 
-public class ThematicAreaAndTypeOfResourceImp {
+import com.example.bibliotecareactiva.application.mappers.ResourceMapper;
+import com.example.bibliotecareactiva.application.useCase.usecaseQueryInterface.ThematicAreaAndTypeOfResource;
+import com.example.bibliotecareactiva.domain.dto.ResourceDTO;
+import com.example.bibliotecareactiva.domain.repository.ResourceRepository;
+import com.example.bibliotecareactiva.domain.valueObject.ThematicArea;
+import com.example.bibliotecareactiva.domain.valueObject.TypeOfResource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ThematicAreaAndTypeOfResourceImp implements ThematicAreaAndTypeOfResource {
+
+    private final ResourceRepository resourceRepository;
+    private final ResourceMapper resourceMapper;
+
+    @Override
+    public Flux<List<ResourceDTO>> recommendThematicAreaAndTypeOfResource(ThematicArea thematicArea, TypeOfResource typeOfResource) {
+        return null;
+    }
 }
