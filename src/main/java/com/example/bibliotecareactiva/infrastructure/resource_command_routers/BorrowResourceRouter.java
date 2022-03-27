b619e5c7857fcb81a63734c3b2cc78d36e586512
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-26 23:21:16 -0500

    se crean rutas en command_routes

--- src/main/java/com/example/bibliotecareactiva/infrastructure/resource_command_routers/BorrowResourceRouter.java
+++ src/main/java/com/example/bibliotecareactiva/infrastructure/resource_command_routers/BorrowResourceRouter.java
@@ -1,7 +1,26 @@
 package com.example.bibliotecareactiva.infrastructure.resource_command_routers;
 
+import com.example.bibliotecareactiva.application.useCaseImp.useCaseCommandImp.BorrowResourceUseCase;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.PUT;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class BorrowResourceRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> borrowResourceRouterFunction(BorrowResourceUseCase borrowResourceUseCase){
+        return route(PUT("api/resource/borrow/{id}"),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(borrowResourceUseCase
+                                .borrowResource(request.pathVariable("id")), String.class))
+        );
+    }
 }
