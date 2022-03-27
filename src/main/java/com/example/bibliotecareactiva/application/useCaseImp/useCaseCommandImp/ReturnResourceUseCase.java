b739fd813636178b2b0255cfca4a0e298d330b0f
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-26 23:06:14 -0500

    se crea returnResourceUseCase

--- src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseCommandImp/ReturnResourceUseCase.java
+++ src/main/java/com/example/bibliotecareactiva/application/useCaseImp/useCaseCommandImp/ReturnResourceUseCase.java
@@ -3,6 +3,7 @@ package com.example.bibliotecareactiva.application.useCaseImp.useCaseCommandImp;
 import com.example.bibliotecareactiva.application.mappers.ResourceMapper;
 import com.example.bibliotecareactiva.application.useCase.useCaseCommandInterface.ReturnResource;
 import com.example.bibliotecareactiva.domain.repository.ResourceRepository;
+import com.example.bibliotecareactiva.domain.valueObject.Availability;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -16,6 +17,12 @@ public class ReturnResourceUseCase implements ReturnResource {
 
     @Override
     public Mono<String> returnResource(String id) {
-        return null;
+        return resourceRepository.findById(id)
+                .flatMap(r -> r.getAvailability().equals(Availability.UNAVAILABLE) ?
+                        resourceRepository.save(resourceMapper
+                                        .mapToCollection().apply(resourceMapper.mapToDTOWhenReturn().apply(r)))
+                                .thenReturn("Recurso devuelto exitosamente")
+                        : Mono.just("El recurso no se encuentra prestado")
+                ).switchIfEmpty(Mono.just("El recurso no pudo ser encontrado"));
     }
 }
